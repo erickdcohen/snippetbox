8e799006b867f7e54b89c87d37ef18f554f5ea1a
Erick David Cohen <erickdcohen@gmail.com>
2026-04-26 21:33:05 -0400

    Adding until csrf

--- internal/models/users.go
+++ internal/models/users.go
@@ -51,7 +51,7 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 	var id int
 	var hashedPassword []byte
 
-	stmt := "SELECT id, hashedPassword FROM users WHERE email = ?"
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
 	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
